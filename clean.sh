2b50d9c4c62e2c2dc865c79988b7669f17a40485
antoniusmisfit <antoniusmisfit@gmail.com>
2017-12-19 17:39:43 -0500

    Small fix to clean.sh

--- clean.sh
+++ clean.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if [[ "$1" == "-a" ]];then
-rm -rf busybox* isoimage kernel* linux* *.iso syslinux*
+rm -rf busybox* isoimage kernel* linux* *.iso syslinux* dhcpcd* links*
 rm hdd.img
 else
-rm -rf busybox-* isoimage kernel/ linux-* syslinux-*
+rm -rf busybox-* isoimage kernel/ linux-* syslinux-* links-* dhcpcd-*
 fi
