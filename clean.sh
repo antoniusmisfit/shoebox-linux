c8e6be6ae1ce5a7473e6d196ffc8d29dab039045
antoniusmisfit <antoniusmisfit@gmail.com>
2017-12-31 02:14:47 -0500

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 export WORK="$(pwd)"
 export ROOTFS="$WORK/rootfs"
 export SRC="$WORK/sources"
 export ISO="$WORK/iso"
 if [[ "$1" == "-a" ]];then
 rm -rf $ROOTFS $SRC $ISO
-rm hdd.img
+rm *.img *.iso
 else
 rm -rf $ROOTFS $ISO $SRC/*-*
 fi
